305cc0e1b21335d41c721f2736944eecb9daa659
Sabaa-Abdennabi <abdennabisabaa02@gmail.com>
2023-11-30 20:37:12 +0100

    Ajoutez des fichiers projet.

---
+++ TP2/Controllers/GenreController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TP2.Models;
+
+namespace TP2.Controllers
+{
+    public class GenreController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public GenreController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            var movies = _db.movies.ToList();
+            return View(movies);
+        }
+    
+
+    }
+}
---
+++ TP2/Models/Movie.cs
@@ -0,0 +1,11 @@
+﻿namespace TP2.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public Movie()
+        {
+        }
+    }
+}
